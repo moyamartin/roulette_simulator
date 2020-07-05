7928d038da790d4ce73066e188a8665e76e457b8
Martin Moya <moyamartin1@gmail.com>
2020-07-05 18:07:32 -0300

    Added missing files

---
+++ inc/roulette_bets.hpp
@@ -0,0 +1,22 @@
+#ifndef ROULETTE_BETS_HPP
+#define ROULETTE_BETS_HPP
+
+#include <map>
+
+enum simple_bets_t { EVEN, ODD, RED, BLACK, MAJOR, MINOR, VALUE };
+
+class RouletteBets 
+{
+public:
+	static void resetResult();
+	static void printResult();
+	static void setResult(int value);
+	static int getResult(simple_bets_t bet);
+	static std::string printBet(simple_bets_t value);
+
+private:
+	static std::map<simple_bets_t, int> simple_bets_result;
+
+};
+
+#endif // ROULETTE_BETS_HPP
---
+++ src/roulette_bets.cpp
@@ -0,0 +1,78 @@
+#include <roulette_bets.hpp>
+#include <iostream>
+#include <string>
+
+std::map<simple_bets_t, int> RouletteBets::simple_bets_result = {
+	{EVEN, 0},
+	{ODD, 0},
+	{RED, 0},
+	{BLACK, 0},
+	{MAJOR, 0},
+	{MINOR, 0},
+	{VALUE, 0}
+};
+
+int RouletteBets::getResult(simple_bets_t value)
+{
+	return simple_bets_result[value];
+}
+
+void RouletteBets::setResult(int random_number)
+{
+	simple_bets_result[VALUE] = random_number;
+	if(random_number > 0) {
+		simple_bets_result[EVEN] = (random_number%2 == 0) ? 1 : 0;
+		simple_bets_result[ODD] = !simple_bets_result[EVEN];
+		if(random_number < 19) {
+			simple_bets_result[MINOR] = 1;
+			if(random_number <= 10) {
+				simple_bets_result[BLACK] = simple_bets_result[EVEN];
+			} else {
+				simple_bets_result[BLACK] = simple_bets_result[ODD];
+			}
+		} else { 
+			simple_bets_result[MINOR] = 0;
+			if(random_number <= 28) {
+				simple_bets_result[BLACK] = simple_bets_result[EVEN];
+			} else {
+				simple_bets_result[BLACK] = simple_bets_result[ODD];
+			}
+		}
+		simple_bets_result[MAJOR] = !simple_bets_result[MINOR];
+		simple_bets_result[RED] = !simple_bets_result[BLACK];
+	}
+}
+
+void RouletteBets::resetResult()
+{
+	for(std::map<simple_bets_t, int>::iterator it = 
+			simple_bets_result.begin();
+			it != simple_bets_result.end(); it++) {
+		it->second = 0;
+	}
+}
+
+void RouletteBets::printResult()
+{
+	std::cout << "------- ROULETTE RESULT" << std::endl;
+	for(std::map<simple_bets_t, int>::iterator it = 
+			simple_bets_result.begin();
+			it != simple_bets_result.end(); it++) {
+		std::cout << printBet(it->first) << ": " << it->second << "\t";
+	}
+	std::cout<<"\n"<< std::endl;
+}
+
+std::string RouletteBets::printBet(simple_bets_t value)
+{
+	switch(value) {
+		case EVEN:	return "EVEN";
+		case ODD:	return "ODD";
+		case RED:	return "RED";
+		case BLACK:	return "BLACK";
+		case MAJOR:	return "MAJOR";
+		case MINOR:	return "MINOR";
+		case VALUE:	return "VALUE";
+		default:	return "VALUE";
+	}
+}

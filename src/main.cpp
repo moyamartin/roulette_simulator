09294e1266607a3bf33661a9e60ba33d7978cbd4
Martin Moya <moyamartin1@gmail.com>
2020-07-06 15:18:48 -0300

    Adds functionality: write log to file

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,18 @@
 #include <chrono>
 #include <cstring>
 #include <iostream>
+#include <fstream>
 
 #include "player.hpp"
 #include "roulette.hpp"
 
 int main(int argc, char* argv[])
 {
-	std::cout << "Welcome to ROULETTE++" << std::endl;
-
-
-	int verbose = 0, plays = 10000, final_balance = 0, time_flag = 0;
+	constexpr  char LOG_FILE_NAME[] = "log.txt";
+	int verbose = 0, plays = 10000, final_balance = 0, time_flag = 0, 
+		log_to_file = 0;
+	std::streambuf *psbuf, *backup = std::cout.rdbuf();
+	std::ofstream logfile;
 
 	for(int i = 0; i < argc; i++){
 		if(!strcmp(argv[i], "-v")){
@@ -22,7 +24,18 @@ int main(int argc, char* argv[])
 		if(!strcmp(argv[i], "-t")) {
 			time_flag = 1;
 		}
+		if(!strcmp(argv[i], "-f")) {
+			log_to_file = 1;
+		}
 	}
+
+	if(log_to_file) {
+		logfile.open(LOG_FILE_NAME, std::ofstream::trunc);
+		psbuf = logfile.rdbuf();
+		std::cout.rdbuf(psbuf);
+	}
+
+	std::cout << "Welcome to ROULETTE++" << std::endl;
 
 	Roulette *roulette = new Roulette(verbose);
 	std::array<Player*, 6> players = {
@@ -33,7 +46,7 @@ int main(int argc, char* argv[])
 		new Player("Player E", EVEN, verbose),
 		new Player("Player F", ODD, verbose)
 	};
-	
+
 	auto t1 = std::chrono::high_resolution_clock::now();
 	for(int i = 0; i < plays; i++) {
 		final_balance = 0;
@@ -49,7 +62,13 @@ int main(int argc, char* argv[])
 
 	auto duration = std::chrono::duration_cast<std::chrono::microseconds>
 		( t2 - t1 ).count();
-	if(time_flag)
+	if(time_flag){
 		std::cout << "Execution time: " << duration << " uS"<< std::endl;
+	}
+	if(log_to_file){
+		std::cout.rdbuf(backup);
+		logfile.close();
+	}
+	
 	return 0;
 }
